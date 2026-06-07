058117ef7a873110c1205cd17626154e8066584e
Jithendra-ayesh <jithendraayesh25@gmail.com>
2026-06-07 11:52:50 +0530

    Implement basic DNS client-server resolver

--- client/dns_client.py
+++ client/dns_client.py
@@ -1,1 +1,16 @@
+import socket
 
+HOST = "127.0.0.1"
+PORT = 5050
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM
+)
+
+client.connect((HOST, PORT))
+domain = input("Enter Domain: ")
+client.send(domain.encode())
+response = client.recv(1024).decode()
+
+print(f"\nResolved IP: {response}")
+
+client.close()
--- server/dns_server.py
+++ server/dns_server.py
@@ -1,1 +1,27 @@
+import socket
 
+HOST = "127.0.0.1"
+PORT = 5050
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen()
+
+print(f"[SERVER] Listening on {HOST}:{PORT}")
+
+client_socket, client_address = server.accept()
+print(f"[CONNECTED] {client_address}")
+
+domain = client_socket.recv(1024).decode()
+
+print(f"[REQUEST] {domain}")
+
+try:
+    ip_address = socket.gethostbyname(domain)
+
+except socket.gaierror:
+    ip_address = "Unable to resolve domain"
+
+client_socket.send(ip_address.encode())
+client_socket.close()
+server.close()
