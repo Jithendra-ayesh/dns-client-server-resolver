8482a7337bc89bc323cdd1206869817f568281d2
Jithendra-ayesh <jithendraayesh25@gmail.com>
2026-06-07 13:29:39 +0530

    Enhanced logging and network accessibility

--- server/dns_server.py
+++ server/dns_server.py
@@ -1,7 +1,8 @@
 import socket
 import threading
+from datetime import datetime
 
-HOST = "127.0.0.1"
+HOST = "0.0.0.0"
 PORT = 5050
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -9,6 +10,22 @@ server.bind((HOST, PORT))
 server.listen()
 
 print(f"[SERVER] Listening on {HOST}:{PORT}")
+
+def log_query(client_ip, query, response):
+
+    timestamp = datetime.now().strftime(
+        "%Y-%m-%d %H:%M:%S"
+    )
+
+    with open("results.txt", "a") as file:
+
+        file.write(
+            f"\n[{timestamp}]\n"
+            f"Client IP : {client_ip}\n"
+            f"Query     : {query}\n"
+            f"Response  : {response}\n"
+            f"{'-'*40}\n"
+        )
 
 def handle_client(client_socket, client_address):
     print(f"[CONNECTED] {client_address}")
@@ -40,6 +57,7 @@ def handle_client(client_socket, client_address):
         except Exception as e:
             response = f"Error: {e}"
 
+        log_query(client_address[0], query, response)
         client_socket.send(response.encode())
     client_socket.close()
     print(f"[DISCONNECTED] {client_address}")
